440f02686a192c5fb67addffa9a77a66cf864194
pranit pawar <82363887+pranit-p@users.noreply.github.com>
2021-06-02 10:04:27 +0530

    Update ExamPage.js

--- src/component/Exam/ExamPage.js
+++ src/component/Exam/ExamPage.js
@@ -17,19 +17,25 @@ const ExamPage = () => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
+           <Alert variant="success">
+            <Alert.Heading>Complete Technical Document Writer Course</Alert.Heading>
+            <p>https://developers.google.com/tech-writing
+                Complete This COurse
+            </p>
+          </Alert>
           <Alert variant="success">
             <Alert.Heading>Become Best English Speaker</Alert.Heading>
             <p>I want to become Best English Speaker</p>
           </Alert>
           <Alert variant="success">
-            <Alert.Heading>Complete Docker Course</Alert.Heading>
+            <Alert.Heading>Hands On with  Docker</Alert.Heading>
             <p>
               I want to complete docker course on interleap.co This Course is
               very important for deployment infrastructure on any cloud
             </p>
           </Alert>
           <Alert variant="success">
-            <Alert.Heading>Complete Serverless Course</Alert.Heading>
+            <Alert.Heading>Hands On Serverless</Alert.Heading>
             <p>
               I want to complete Serverless course on serverless framework
               official website. This course is very useful for deploy react app
@@ -37,28 +43,25 @@ const ExamPage = () => {
             </p>
           </Alert>
           <Alert variant="success">
-            <Alert.Heading>Complete Atlantis Course</Alert.Heading>
+            <Alert.Heading>Hands On  Atlantis</Alert.Heading>
             <p>
               I want to complete Atlantis Course it's help for setup atlantis
               cloud.
             </p>
           </Alert>
           <Alert variant="success">
-            <Alert.Heading>Complete DevOps Course</Alert.Heading>
-            <p>
-              I want to complete Atlantis Course it's help for setup atlantis
-              cloud.
-            </p>
-          </Alert>
-          <Alert variant="success">
-            <Alert.Heading>Hands On  in AWS</Alert.Heading>
+            <Alert.Heading>Hands On with All AWS Services AWS</Alert.Heading>
             <p>I want to Hands On  in AWS.</p>
           </Alert>
           <Alert variant="success">
-            <Alert.Heading>Hands On in  Terragrunt</Alert.Heading>
+            <Alert.Heading>Hands On with Terragrunt</Alert.Heading>
             <p>
               TERRAGRUNT is very helpful to keep your terraform code dry
             </p>
+          </Alert>
+          <Alert variant="success">
+            <Alert.Heading>Hands on In Github Action</Alert.Heading>
+            <p>I want to complete Nodejs course on Udemy</p>
           </Alert>
           <Alert variant="success">
             <Alert.Heading>Complete React Course</Alert.Heading>
@@ -68,17 +71,7 @@ const ExamPage = () => {
             <Alert.Heading>Complete Node.js Course</Alert.Heading>
             <p>I want to complete Nodejs course on Udemy</p>
           </Alert>
-          <Alert variant="success">
-            <Alert.Heading>Hands on In Github Action</Alert.Heading>
-            <p>I want to complete Nodejs course on Udemy</p>
-          </Alert>
-            <Alert variant="success">
-            <Alert.Heading>Complete Technical Document Writer Course</Alert.Heading>
-            <p>https://developers.google.com/tech-writing
-                Complete This COurse
-            
-            </p>
-          </Alert>
+           
     
       
         </Modal.Body>
